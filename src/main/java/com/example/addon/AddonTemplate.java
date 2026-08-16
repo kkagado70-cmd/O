94800c86aab62d2f52763ea671d2fdc102ef75f4
kkagado70-cmd <kkagado70@gmail.com>
2026-08-16 14:33:37 -0300

    Update AddonTemplate.java

--- src/main/java/com/example/addon/AddonTemplate.java
+++ src/main/java/com/example/addon/AddonTemplate.java
@@ -1,40 +1,29 @@
 package com.example.addon;
 
-import com.example.addon.commands.CommandExample;
-import com.example.addon.hud.HudExample;
-import com.example.addon.modules.ModuleExample;
-import com.mojang.logging.LogUtils;
-import meteordevelopment.meteorclient.addons.GithubRepo;
+import com.example.addon.modules.XbowCart;
 import meteordevelopment.meteorclient.addons.MeteorAddon;
-import meteordevelopment.meteorclient.commands.Commands;
-import meteordevelopment.meteorclient.systems.hud.Hud;
-import meteordevelopment.meteorclient.systems.hud.HudGroup;
 import meteordevelopment.meteorclient.systems.modules.Category;
 import meteordevelopment.meteorclient.systems.modules.Modules;
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class AddonTemplate extends MeteorAddon {
-    public static final Logger LOG = LogUtils.getLogger();
-    public static final Category CATEGORY = new Category("Example");
-    public static final HudGroup HUD_GROUP = new HudGroup("Example");
+    public static final Logger LOG = LoggerFactory.getLogger("XbowAddon");
+    public static final Category CATEGORY = new Category("CPVP");
 
     @Override
     public void onInitialize() {
-        LOG.info("Initializing Meteor Addon Template");
+        LOG.info("Inicializando Xbow Cart Addon...");
 
-        // Modules
-        Modules.get().add(new ModuleExample());
-
-        // Commands
-        Commands.add(new CommandExample());
-
-        // HUD
-        Hud.get().register(HudExample.INFO);
+        // Registra o seu modulo dentro do Meteor Client
+        Modules.get().add(new XbowCart());
     }
 
     @Override
-    public void onRegisterCategories() {
-        Modules.registerCategory(CATEGORY);
+    public String getPackage() {
+        return "com.example.addon";
+    }
+}        Modules.registerCategory(CATEGORY);
     }
 
     @Override
